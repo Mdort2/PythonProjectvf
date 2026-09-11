839689a9d30fd6f9fb2bc971ea1f2e4b8fb40924
Mdort2 <mdort2@mail.valenciacollege.edu>
2026-09-11 00:25:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print ("Hello World")
+# 1. Multiplication (5 multiplied by 6)
+print("Please enter the first number")
+num1 = int(input())
+print("Please enter the second number")
+num2 = int(input())
+answer = num1 * num2
+print("The answer to your query is:", answer)
+
+# 2. Regular Division (8 divided by 3)
+print("Please enter the first number")
+num1 = int(input())
+print("Please enter the second number")
+num2 = int(input())
+answer = num1 / num2
+print("The answer to your query is:", answer)
+
+# 3. Floor Division (8 divided by 3 without the remainder)
+print("Please enter the first number")
+num1 = int(input())
+print("Please enter the second number")
+num2 = int(input())
+answer = num1 // num2
+print("The answer to your query is:", answer)
+
+# 4. Modulo (8 modulo 3 / remainder only)
+print("Please enter the first number")
+num1 = int(input())
+print("Please enter the second number")
+num2 = int(input())
+answer = num1 % num2
+print("The answer to your query is:", answer)
+
+# 5. Addition (4200000 add 23090)
+print("Please enter the first number")
+num1 = int(input())
+print("Please enter the second number")
+num2 = int(input())
+answer = num1 + num2
+print("The answer to your query is:", answer)
+
+# 6. Subtraction (4200000 subtract 7633)
+print("Please enter the first number")
+num1 = int(input())
+print("Please enter the second number")
+num2 = int(input())
+answer = num1 - num2
+print("The answer to your query is:", answer)
+
+# Last line requirement
+print("Your Name")
